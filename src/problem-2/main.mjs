911368ca3b17e4551c19ca683976a6b6970ce4a1
Simon Plattner <5527585+simonplattner@users.noreply.github.com>
2022-08-14 12:00:13 +0200

    Problem 2 solved

--- src/problem-2/main.mjs
+++ src/problem-2/main.mjs
@@ -3,8 +3,7 @@ import stream from "./modules/stream.mjs";
 
 async function main() {
 
-  const sum = stream([1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89])
-  // const sum = stream(fibbonaciGenerator)
+  const sum = stream(fibbonaciGenerator())
     .takeWhile(s => s < 4000000)
     .filter(s => s % 2 === 0)
     .reduce((a, b) => a + b, 0);
